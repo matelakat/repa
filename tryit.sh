021fde5de920b121534603fa6af04114dfd693ea
Mate Lakat <mlakat@mlakat.hu>
2013-08-05 19:28:59 +0100

    Updated the kernel

--- tryit.sh
+++ tryit.sh
@@ -7,14 +7,15 @@ WORKSPACE=.workspace
 BUILDROOT_VER=buildroot-2013.05
 LOCAL_KERNEL="$THIS_DIR/vmlinuz"
 
-KERNEL_FILE=boot/vmlinuz-3.2.37-030237-virtual
-KERNEL_DEB=linux-image-3.2.37-030237-virtual_3.2.37-030237.201301152335_amd64.deb
+KERNEL_DEB=linux-image-3.2.48-030248-virtual_3.2.48-030248.201306291335_amd64.deb
+KERNEL_URL="http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.2.48-precise/$KERNEL_DEB"
+KERNEL_FILE=boot/vmlinuz-3.2.48-030248-virtual
 
 if ! [ -e "$LOCAL_KERNEL" ]; then
     TMPDIR=$(mktemp -d)
     (
         cd "$TMPDIR"
-        wget -q http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.2.37-precise/$KERNEL_DEB
+        wget -q "$KERNEL_URL"
         ar vx $KERNEL_DEB
         tar -xjvf data.tar.bz2
         mv "$KERNEL_FILE" "$LOCAL_KERNEL"
