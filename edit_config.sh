48c83f237f7de32dfb120be3fe1befff11a2bbca
Mate Lakat <mlakat@mlakat.hu>
2013-08-06 07:17:13 +0100

    Add an overlay directory

--- edit_config.sh
+++ edit_config.sh
@@ -7,6 +7,7 @@ WORKSPACE=.workspace
 BUILDROOT_VER=buildroot-2013.05
 CROSSTOOL_DIR="$THIS_DIR/.crosstool"
 XTOOLS_DIR="$CROSSTOOL_DIR/x-tools"
+OVERLAY_DIR="$THIS_DIR/overlay"
 
 # Copy over the packages
 find packages/ -maxdepth 1 -mindepth 1 -type d | while read pkgpath; do
@@ -20,8 +21,11 @@ patch -p 0 --forward < package_config.patch || true
     cd "$WORKSPACE/$BUILDROOT_VER"
     make defconfig BR2_DEFCONFIG="$THIS_DIR/repa_defconfig"
     sed -ie "s,/data/matelakat/crosstoolng/x-tools,$XTOOLS_DIR,g" .config
+    sed -ie "s,overlay_directory_comes_here,$OVERLAY_DIR,g" .config
     make menuconfig
     sed -ie "s,$XTOOLS_DIR,/data/matelakat/crosstoolng/x-tools,g" .config
+    sed -ie "s,$OVERLAY_DIR,overlay_directory_comes_here,g" .config
     make savedefconfig BR2_DEFCONFIG="$THIS_DIR/repa_defconfig"
     sed -ie "s,/data/matelakat/crosstoolng/x-tools,$XTOOLS_DIR,g" .config
+    sed -ie "s,overlay_directory_comes_here,$OVERLAY_DIR,g" .config
 )
