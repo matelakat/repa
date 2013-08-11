20cdd5430ff9fb5d2de3928aaf944a679d667f91
Mate Lakat <mlakat@mlakat.hu>
2013-08-11 16:43:23 +0100

    custom uwsgi branch with hacks
    As uwsgi had issues with self-configuring herself in a cross
    environment, a separate fork has been created that contains tweaks.

--- packages/uwsgi/uwsgi.mk
+++ packages/uwsgi/uwsgi.mk
@@ -1,16 +1,25 @@
-UWSGI_VERSION = 1.9.14
-UWSGI_SOURCE = uwsgi-$(UWSGI_VERSION).tar.gz
-UWSGI_SITE = http://projects.unbit.it/downloads
+UWSGI_VERSION = python-config-fix
+UWSGI_SITE = git://github.com/matelakat/uwsgi
 UWSGI_LICENSE = GPL2
 UWSGI_LICENSE_FILES = LICENSE
 
 define UWSGI_BUILD_CMDS
-    (cd $(@D); PATH="$(HOST_DIR)/usr/x86_64-buildroot-linux-gnu/sysroot/usr/bin:$(PATH)" UWSGI_INCLUDES="$(TARGET_INCLUDES)" CC="$(TARGET_CC)" CPP="$(TARGET_CPP)" $(HOST_DIR)/usr/bin/python uwsgiconfig.py --build)
+    (cd $(@D) && \
+    UWSGICONFIG_PYTHONCONFIG=$(@D)/buildroot-python-config \
+    PYTHON_VERSION_MAJOR=$(PYTHON_VERSION_MAJOR) \
+    BUILD_DIR=$(BUILD_DIR) \
+    PYTHON_VERSION=$(PYTHON_VERSION) \
+    UWSGI_PYTHON_NOLIB="True" \
+    PATH="$(BUILD_DIR)/pcre-8.32:$(BUILD_DIR)/libxml2-2.9.0:${PATH}" \
+    UWSGI_INCLUDES="$(TARGET_INCLUDES)" \
+    CC="$(TARGET_CC)" \
+    CPP="$(TARGET_CPP)" \
+    $(HOST_DIR)/usr/bin/python uwsgiconfig.py --build)
 endef
 
 define UWSGI_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/uwsgi $(TARGET_DIR)/usr/bin
-    $(INSTALL) -d -m 0755 -o uwsgi -g uwsgi $(TARGET_DIR)/var/uwsgi
+    $(INSTALL) -d -m 0755 $(TARGET_DIR)/var/uwsgi
 endef
 
 define UWSGI_USERS
